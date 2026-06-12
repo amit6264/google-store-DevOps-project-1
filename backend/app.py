6097d417e56f1577e06cbb48cc84bb4516addd40
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-12 14:01:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,9 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "eks-rds-db.cza4ia2egu29.eu-north-1.rds.amazonaws.com",
+    "host": "database-1.c5u0a2qysvfs.eu-north-1.rds.amazonaws.com",
     "user": "admin",
-    "password": "Admin12345!",
+    "password": "cloud123",
     "database": "cloud"
 }
 
